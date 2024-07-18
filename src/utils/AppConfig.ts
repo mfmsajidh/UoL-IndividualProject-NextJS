4a9d1dd310bf2f5a3deda4095fb6ed63cb02cd08
Sajidh Farook <36571307+mfmsajidh@users.noreply.github.com>
2024-07-18 15:14:14 +0100

    chore(todo): completed todos

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -2,7 +2,6 @@ import type { LocalePrefix } from 'node_modules/next-intl/dist/types/src/routing
 
 const localePrefix: LocalePrefix = 'as-needed';
 
-// FIXME: Update this configuration file based on your project information
 export const AppConfig = {
   name: 'CV Mate',
   locales: ['en', 'fr'],
