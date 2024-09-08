55180662b53065e24c774fd892e07d9442e1e8a6
Sajidh Farook <36571307+mfmsajidh@users.noreply.github.com>
2024-09-08 21:02:10 +0100

    refactor(pinata): improve pinata route logic

--- src/app/[locale]/(auth)/api/pinata/route.ts
+++ src/app/[locale]/(auth)/api/pinata/route.ts
@@ -5,12 +5,6 @@ import type { SignedUrlOptions } from 'pinata';
 import { logger } from '@/libs/Logger';
 import { pinata } from '@/libs/Pinata';
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 export const POST = async (request: NextRequest) => {
   try {
     const data = await request.formData();
@@ -18,9 +12,11 @@ export const POST = async (request: NextRequest) => {
 
     return NextResponse.json(uploadData, { status: 200 });
   } catch (error) {
-    logger.error(error, 'An error occurred while creating a pinata content');
-
-    return NextResponse.json({}, { status: 500 });
+    logger.error(error, 'An error occurred while creating a Pinata content');
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
   }
 };
 
@@ -28,14 +24,22 @@ export const GET = async (request: NextRequest) => {
   try {
     const { searchParams } = request.nextUrl;
     const cid = searchParams.get('cid');
+
+    if (!cid) {
+      return NextResponse.json({ error: 'CID is required' }, { status: 400 });
+    }
+
     const url = await pinata.gateways.createSignedURL(<SignedUrlOptions>{
       cid,
-      expires: 3600,
+      expires: 3600, // URL expires in 1 hour
     });
 
-    return NextResponse.json(url, { status: 200 });
+    return NextResponse.json({ url }, { status: 200 });
   } catch (error) {
-    logger.error(error, 'An error occurred while retrieving a pinata content');
-    return NextResponse.json({}, { status: 500 });
+    logger.error(error, 'An error occurred while retrieving a Pinata content');
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
   }
 };
