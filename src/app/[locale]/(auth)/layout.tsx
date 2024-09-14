113984cc0a0169d5da3be8bbe98c535b7dfab71d
Sajidh Farook <36571307+mfmsajidh@users.noreply.github.com>
2024-09-14 15:56:55 +0100

    feat(): added extra localisation to auth flow

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,4 +1,27 @@
-import { enUS, frFR } from '@clerk/localizations';
+import {
+  arSA,
+  csCZ,
+  daDK,
+  deDE,
+  elGR,
+  enUS,
+  esES,
+  frFR,
+  heIL,
+  itIT,
+  jaJP,
+  koKR,
+  nbNO,
+  nlNL,
+  plPL,
+  ptPT,
+  ruRU,
+  svSE,
+  trTR,
+  ukUA,
+  viVN,
+  zhCN,
+} from '@clerk/localizations';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { ReactNode } from 'react';
 
@@ -11,8 +34,73 @@ export default function AuthLayout(props: {
   let signUpUrl = '/sign-up';
   let dashboardUrl = '/dashboard';
 
-  if (props.params.locale === 'fr') {
-    clerkLocale = frFR;
+  switch (props.params.locale) {
+    case 'ar':
+      clerkLocale = arSA;
+      break;
+    case 'cs':
+      clerkLocale = csCZ;
+      break;
+    case 'da':
+      clerkLocale = daDK;
+      break;
+    case 'de':
+      clerkLocale = deDE;
+      break;
+    case 'el':
+      clerkLocale = elGR;
+      break;
+    case 'es':
+      clerkLocale = esES;
+      break;
+    case 'fr':
+      clerkLocale = frFR;
+      break;
+    case 'he':
+      clerkLocale = heIL;
+      break;
+    case 'it':
+      clerkLocale = itIT;
+      break;
+    case 'ja':
+      clerkLocale = jaJP;
+      break;
+    case 'ko':
+      clerkLocale = koKR;
+      break;
+    case 'nl':
+      clerkLocale = nlNL;
+      break;
+    case 'no':
+      clerkLocale = nbNO;
+      break;
+    case 'pl':
+      clerkLocale = plPL;
+      break;
+    case 'pt':
+      clerkLocale = ptPT;
+      break;
+    case 'ru':
+      clerkLocale = ruRU;
+      break;
+    case 'sv':
+      clerkLocale = svSE;
+      break;
+    case 'tr':
+      clerkLocale = trTR;
+      break;
+    case 'uk':
+      clerkLocale = ukUA;
+      break;
+    case 'vi':
+      clerkLocale = viVN;
+      break;
+    case 'zh':
+      clerkLocale = zhCN;
+      break;
+    default:
+      clerkLocale = enUS;
+      break;
   }
 
   if (props.params.locale !== 'en') {
